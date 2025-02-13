04517a1e67f6be9eaa68386a259c702c2e16e0bf
juliusalberto <u7724204@anu.edu.au>
2025-02-13 22:00:47 +1100

    [RED] add test for keep alive and pipelining

--- tests/test.py
+++ tests/test.py
@@ -28,7 +28,6 @@ class TestHTTPServer(unittest.TestCase):
             )
             s.sendall(test_request.encode())
             
-            # Read response status line
             f = s.makefile('rb')
             response_line = f.readline().decode()
             print(response_line)
@@ -57,7 +56,7 @@ class TestHTTPServer(unittest.TestCase):
             self.assertIsNotNone(content_length, "No Content-Length header found")
             
             # Read response body
-            content = s.recv(content_length)
+            content = f.read(content_length)
             
             # Compare with expected content
             with open(self.test_file, 'rb') as f:
@@ -110,6 +109,72 @@ class TestHTTPServer(unittest.TestCase):
             
             self.assertEqual(status_code, "403")
             self.assertEqual(status_text.strip(), "Forbidden")
+
+    def test_keep_alive(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((self.HOST, self.PORT))
+            
+            # First request
+            test_request = (
+                "GET /tests/resources/test_send.txt HTTP/1.1\r\n"
+                "Host: www.example.com\r\n"
+                "Connection: keep-alive\r\n"
+                "\r\n"
+            )
+            s.sendall(test_request.encode())
+            
+            # Read first response
+            f = s.makefile('rb')
+            self._read_and_verify_response(f)
+            
+            # Second request on same connection
+            s.sendall(test_request.encode())
+            self._read_and_verify_response(f)
+
+    def test_pipelining(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((self.HOST, self.PORT))
+            
+            # Send multiple requests without waiting for responses
+            test_request = (
+                "GET /tests/resources/test_send.txt HTTP/1.1\r\n"
+                "Host: www.example.com\r\n"
+                "Connection: keep-alive\r\n"
+                "\r\n"
+            )
+            
+            # Send 3 requests back to back
+            s.sendall(test_request.encode() * 3)
+            
+            # Read all responses
+            f = s.makefile('rb')
+            for _ in range(3):
+                self._read_and_verify_response(f)
+
+    def _read_and_verify_response(self, f):
+        # Helper method to read and verify a response
+        response_line = f.readline().decode()
+        protocol, status_code, status_text = response_line.split(' ', 2)
+        
+        self.assertEqual(protocol, "HTTP/1.1")
+        self.assertEqual(status_code, "200")
+        
+        content_length = None
+        while True:
+            line = f.readline().decode().strip()
+            if not line:
+                break
+            if ':' in line:
+                key, value = line.split(':', 1)
+                if key.lower() == 'content-length':
+                    content_length = int(value.strip())
+        
+        self.assertIsNotNone(content_length)
+        content = f.read(content_length)
+        
+        with open(self.test_file, 'rb') as test_f:
+            expected_content = test_f.read()
+        self.assertEqual(content, expected_content)
 
 
 if __name__ == '__main__':
